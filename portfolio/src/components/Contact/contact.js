1b4c45369fc95db964fc172a50b61b1950e7d1b0
Neetha Francis <neethafrancis26@gmail.com>
2024-04-18 13:16:24 +0530

    contact

---
+++ portfolio/src/components/Contact/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './contact.css';
+const Contact = () => {
+  return (
+    <section id="contactPage">
+        <h1 className="contactPageTitle">Contact Me</h1>
+
+    </section>
+    
+  ); 
+}
+
+export default Contact
