3629d569d98e8bb4cff01949e0dd9c35ca42b5e3
Neetha Francis <neethafrancis26@gmail.com>
2024-04-18 15:59:12 +0530

    contact

--- portfolio/src/components/Contact/contact.js
+++ portfolio/src/components/Contact/contact.js
@@ -4,7 +4,17 @@ const Contact = () => {
   return (
     <section id="contactPage">
         <h1 className="contactPageTitle">Contact Me</h1>
-
+        <span className='contactDesc'>Fill The Form </span>
+        <form className="contactForm">
+            <input type="text" className="name" placeholder="Your Name"/>
+            <input type="email" className="email" placeholder="Your email"/>
+            <textarea rows="5" cols="30" name="message" placeholder="message" className="msg"></textarea>
+            <button className="submitBtn" type="submit" value="send" >Submit</button>
+            <div>
+                <img src=" " at=" facebook" className="link" /> 
+                <img src=" " at="instagram "className="link" />
+            </div>
+        </form>
     </section>
     
   ); 
