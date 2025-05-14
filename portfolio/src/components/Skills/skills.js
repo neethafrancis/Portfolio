433240ffce8af54553d1f39109349e25066443ca
neethafrancis <77440093+neethafrancis@users.noreply.github.com>
2025-05-14 10:50:17 +0530

    Update skills.js

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import './skills.css';
 import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
 import img from'../../assets/pic4.jpg';
 
 const Skills = () => {
